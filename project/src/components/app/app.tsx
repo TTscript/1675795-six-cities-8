c4bb64fdc15418d22c4e9be695f19024bce6639d
Timur <t.tokumov@gmail.com>
2021-10-07 12:39:46 +0600

    Completed module3-task1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,41 @@
 import MainPage from '../main-page/main-page';
+import LoginPage from '../login-page/login-page';
+import FavoritesPage from '../favorites-page/favorites-page';
+import RoomPage from '../room-page/room-page';
+import Page404 from '../page-404/page-404';
 import { AppScreenProps } from './types';
+import { Switch, Route, BrowserRouter} from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import PrivateRoute from '../private-route/private-route';
 
 function App({offersCount}: AppScreenProps): JSX.Element {
-  return <MainPage offersCount={offersCount}/>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <MainPage
+            offersCount={offersCount}
+          />
+        </Route>
+        <Route exact path={AppRoute.SignIn}>
+          <LoginPage />
+        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.Favorites}
+          render={() => <FavoritesPage />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
+        <Route exact path={AppRoute.Room}>
+          <RoomPage />
+        </Route>
+        <Route>
+          <Page404 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- project/src/components/favorites-page/favorites-page.tsx
+++ project/src/components/favorites-page/favorites-page.tsx
@@ -1,3 +1,5 @@
+import Logo from '../logo/logo';
+
 function FavoritesPage(): JSX.Element {
   return (
     <div className="page">
@@ -5,14 +7,12 @@ function FavoritesPage(): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
+              <Logo />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href=" ">
+                  <a className="header__nav-link header__nav-link--profile" href="/favorites">
                     <div className="header__avatar-wrapper user__avatar-wrapper">
                     </div>
                     <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
@@ -156,9 +156,7 @@ function FavoritesPage(): JSX.Element {
         </div>
       </main>
       <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
-          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
-        </a>
+        <Logo />
       </footer>
     </div>);
 }
